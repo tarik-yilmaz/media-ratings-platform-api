8688b99505ec53164d701244a6dc89a1f4d31fa6
Tarik Yilmaz <tarik.yilmaz.email@gmail.com>
2025-12-11 11:30:59 +0100

    Added RegisterRequest

---
+++ src/main/java/at/technikum/mrp/dto/RegisterRequest.java
@@ -0,0 +1,25 @@
+package at.technikum.mrp.dto;
+
+public class RegisterRequest {
+    private String username;
+    private String password;
+    private String email;
+
+    public RegisterRequest() {}
+
+    public RegisterRequest(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    // Getter und Setter
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+}
