7bb048d2a3ce28aaee29143b6040715c3e1c74fa
Tarik Yilmaz <tarik.yilmaz.email@gmail.com>
2025-12-11 11:30:22 +0100

    added LoginRequest

---
+++ src/main/java/at/technikum/mrp/dto/LoginRequest.java
@@ -0,0 +1,21 @@
+package at.technikum.mrp.dto;
+
+public class LoginRequest {
+    private String username;
+    private String password;
+
+    // Default constructor for JSON deserialization
+    public LoginRequest() {}
+
+    public LoginRequest(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    // Getter und Setter
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+}
