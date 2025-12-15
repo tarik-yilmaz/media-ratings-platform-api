1664c0219bb38b95c29c3c60db8c42c163e9dda2
Tarik Yilmaz <tarik.yilmaz.email@gmail.com>
2025-12-15 18:02:03 +0100

    added ApiException.java

---
+++ src/main/java/at/technikum/mrp/util/ApiException.java
@@ -0,0 +1,20 @@
+package at.technikum.mrp.util;
+
+public class ApiException extends RuntimeException {
+    private final int status;
+
+    public ApiException(int status, String message) {
+        super(message);
+        this.status = status;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public static ApiException badRequest(String msg) { return new ApiException(400, msg); }
+    public static ApiException unauthorized(String msg) { return new ApiException(401, msg); }
+    public static ApiException forbidden(String msg) { return new ApiException(403, msg); }
+    public static ApiException notFound(String msg) { return new ApiException(404, msg); }
+    public static ApiException conflict(String msg) { return new ApiException(409, msg); }
+}
