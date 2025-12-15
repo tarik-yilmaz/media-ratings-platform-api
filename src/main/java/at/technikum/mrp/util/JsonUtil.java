daad9502d314a5ec98747ec22eb7ee25e1a10c84
Tarik Yilmaz <tarik.yilmaz.email@gmail.com>
2025-12-15 18:03:17 +0100

    added JsonUtil

---
+++ src/main/java/at/technikum/mrp/util/JsonUtil.java
@@ -0,0 +1,14 @@
+package at.technikum.mrp.util;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+public class JsonUtil {
+    public static final ObjectMapper MAPPER = new ObjectMapper();
+
+    static {
+        MAPPER.registerModule(new JavaTimeModule());
+        MAPPER.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    }
+}
