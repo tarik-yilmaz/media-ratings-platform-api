69cb6a2978a711d2615515fde43fb0be0c8b86be
Tarik Yilmaz <tarik.yilmaz.email@gmail.com>
2025-12-15 09:24:58 +0100

    Update RatingRequest.java

--- src/main/java/at/technikum/mrp/dto/RatingRequest.java
+++ src/main/java/at/technikum/mrp/dto/RatingRequest.java
@@ -1,22 +1,17 @@
 package at.technikum.mrp.dto;
 
 public class RatingRequest {
-    private Integer mediaId;
-    private Integer stars;
+    private Integer stars; // 1-5
     private String comment;
 
     public RatingRequest() {}
 
-    public RatingRequest(Integer mediaId, Integer stars, String comment) {
-        this.mediaId = mediaId;
+    public RatingRequest(Integer stars, String comment) {
         this.stars = stars;
         this.comment = comment;
     }
 
     // Getter und Setter
-    public Integer getMediaId() { return mediaId; }
-    public void setMediaId(Integer mediaId) { this.mediaId = mediaId; }
-
     public Integer getStars() { return stars; }
     public void setStars(Integer stars) { this.stars = stars; }
 
