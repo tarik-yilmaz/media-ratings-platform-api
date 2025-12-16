819fd7607f64f4202398139c720381220d6689bd
Tarik Yilmaz <tarik.yilmaz.email@gmail.com>
2025-12-16 07:32:35 +0100

    update JsonUtil

--- src/main/java/at/technikum/mrp/util/JsonUtil.java
+++ src/main/java/at/technikum/mrp/util/JsonUtil.java
@@ -4,11 +4,18 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
+/**
+ * Zentraler ObjectMapper für JSON (Jackson).
+ * Wir konfigurieren hier auch LocalDateTime Support, damit createdAt sauber als string rauskommt.
+ */
 public class JsonUtil {
     public static final ObjectMapper MAPPER = new ObjectMapper();
 
     static {
+        // Support für Java Time API (LocalDateTime, Instant, ...)
         MAPPER.registerModule(new JavaTimeModule());
+
+        // Sonst würden Dates als "Timestamp Arrays" ausgegeben werden -> unlesbar im JSON
         MAPPER.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
     }
 }
