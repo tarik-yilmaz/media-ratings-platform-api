5ebdc8723d21430e94cfbae8a5414dc1a96bd6c6
Tarik Yilmaz <tarik.yilmaz.email@gmail.com>
2025-12-16 07:31:14 +0100

    update ApiException

--- src/main/java/at/technikum/mrp/util/ApiException.java
+++ src/main/java/at/technikum/mrp/util/ApiException.java
@@ -1,5 +1,9 @@
 package at.technikum.mrp.util;
 
+/**
+ * Eigene Exception für API-Fehler mit HTTP-Statuscode.
+ * Controller fängt das ab und gibt saubere JSON-Fehlermeldungen zurück (statt Stacktrace).
+ */
 public class ApiException extends RuntimeException {
     private final int status;
 
@@ -8,10 +12,14 @@ public class ApiException extends RuntimeException {
         this.status = status;
     }
 
+    /**
+     * HTTP Statuscode, der an den Client zurückgegeben werden soll.
+     */
     public int getStatus() {
         return status;
     }
 
+    // Convenience-Methoden, damit man im Code kurz und sauber bleibt
     public static ApiException badRequest(String msg) { return new ApiException(400, msg); }
     public static ApiException unauthorized(String msg) { return new ApiException(401, msg); }
     public static ApiException forbidden(String msg) { return new ApiException(403, msg); }
