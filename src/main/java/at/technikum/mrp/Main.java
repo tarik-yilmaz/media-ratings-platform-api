9f3ec0537ead5bc1f399464e58cf3dab969621b1
Tarik Yilmaz <tarik.yilmaz.email@gmail.com>
2026-01-20 15:02:01 +0100

    update Main.java

--- src/main/java/at/technikum/mrp/Main.java
+++ src/main/java/at/technikum/mrp/Main.java
@@ -4,11 +4,14 @@ import at.technikum.mrp.config.DatabaseConfig;
 import at.technikum.mrp.config.ServerConfig;
 import at.technikum.mrp.controller.AuthController;
 import at.technikum.mrp.controller.MediaController;
+import at.technikum.mrp.controller.RatingController;
 import at.technikum.mrp.repository.MediaRepository;
+import at.technikum.mrp.repository.RatingRepository;
 import at.technikum.mrp.repository.UserRepository;
 import at.technikum.mrp.server.MrpHttpServer;
 import at.technikum.mrp.service.AuthService;
 import at.technikum.mrp.service.MediaService;
+import at.technikum.mrp.service.RatingService;
 import at.technikum.mrp.service.TokenService;
 
 public class Main {
@@ -21,21 +24,29 @@ public class Main {
             return;
         }
 
-        // Repos
+        // Repositories
         UserRepository userRepository = new UserRepository();
         MediaRepository mediaRepository = new MediaRepository();
+        RatingRepository ratingRepository = new RatingRepository();
 
         // Services
         TokenService tokenService = new TokenService();
         AuthService authService = new AuthService(userRepository, tokenService);
         MediaService mediaService = new MediaService(mediaRepository);
+        RatingService ratingService = new RatingService(ratingRepository, mediaRepository, userRepository);
 
         // Controller
         AuthController authController = new AuthController(authService);
-        MediaController mediaController = new MediaController(mediaService, tokenService);
+        MediaController mediaController = new MediaController(mediaService, tokenService, ratingService);
+        RatingController ratingController = new RatingController(ratingService, tokenService);
 
         // Server
-        MrpHttpServer server = new MrpHttpServer(ServerConfig.getPort(), authController, mediaController);
+        MrpHttpServer server = new MrpHttpServer(
+                ServerConfig.getPort(),
+                authController,
+                mediaController,
+                ratingController
+        );
         server.start();
 
         System.out.println("Base URL: http://localhost:" + ServerConfig.getPort() + "/api");
